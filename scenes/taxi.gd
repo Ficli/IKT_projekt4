6a4eed1e585c6eea02ab2cc0ed86c3516578d658
Lobster <polyak.daavid@gmail.com>
2024-05-16 22:46:18 +0200

    Default Taxi
    Movement setup

--- scenes/taxi.gd
+++ scenes/taxi.gd
@@ -1,35 +1,40 @@
 extends CharacterBody2D
-
-
-const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
 @onready var sprite_2d = $Sprite2D
 
-# Get the gravity from the project settings to be synced with RigidBody nodes.
-var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+var DefSpeed = 250
+var Accelerate = DefSpeed / 10
+var Break = DefSpeed / 30
+#var HandBreak
+var directions = {
+	"Up": {"state": false, "speed": ["y", -DefSpeed]},
+	"Left": {"state": false, "speed": ["x", -DefSpeed]},
+	"Down": {"state": false, "speed": ["y", DefSpeed]},
+	"Right": {"state": false, "speed": ["x", DefSpeed]}
+}
 
 
 func _physics_process(delta):
-	# Add the gravity.
-	if not is_on_floor():
-		velocity.y += gravity * delta
-		sprite_2d.animation = "going_back"
-
-	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
-
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction = Input.get_axis("ui_left", "ui_right")
-	if direction:
-		velocity.x = direction * SPEED
-		sprite_2d.animation = "going_left"
-	else:
-		velocity.x = move_toward(velocity.x, 0, 5)
-
-	move_and_slide()
-
-	var isleft = velocity.x > 0
-	sprite_2d.flip_h = isleft
 	
+	#Handle Inputs
+	for key in directions:
+		
+		var axis = directions[key]["speed"][0]
+		
+		#Get the pressed and released buttons
+		if Input.is_action_just_pressed(key):
+			directions[key]["state"] = true
+		if Input.is_action_just_released(key):
+			directions[key]["state"] = false
+	
+		#Accelerate if the key is pressed
+		if directions[key]["state"]:
+			var speed = directions[key]["speed"][1]
+			velocity[axis] = move_toward(velocity[axis], speed, 10)
+	
+	# Break if the keys are not pressed
+	if not directions["Up"]["state"] and not directions["Down"]["state"]:
+		velocity.y = move_toward(velocity.y, 0, Break)
+	if not directions["Left"]["state"] and not directions["Right"]["state"]:
+		velocity.x = move_toward(velocity.x, 0, Break)
+	
+	move_and_slide()
