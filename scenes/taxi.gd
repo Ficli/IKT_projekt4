89b47faa396ffae397970be023806b86d269ec11
Davidvoklol <polyak.daavid@gmail.com>
2024-05-17 13:50:09 +0200

    commit

--- scenes/taxi.gd
+++ scenes/taxi.gd
@@ -1,41 +1,37 @@
 extends CharacterBody2D
 @onready var sprite_2d = $Sprite2D
 
-var DefSpeed = 250
-var Accelerate = DefSpeed / 10
-var Break = DefSpeed / 30
-#var HandBreak
-var directions = {
-	"Up": {"state": false, "speed": ["y", -DefSpeed]},
-	"Left": {"state": false, "speed": ["x", -DefSpeed]},
-	"Down": {"state": false, "speed": ["y", DefSpeed]},
-	"Right": {"state": false, "speed": ["x", DefSpeed]}
-}
+func keyDown(key):
+	return Input.is_action_pressed(key)
 
+var speed = 300
+var speedScale = 1
+var acceleration = 10
+var deceleration = 5
+var movementScale = 1
 
 func _physics_process(delta):
 	
+	if velocity.x != 0 and velocity.y != 0:
+		movementScale = 2
+		speedScale = move_toward(speedScale, 0.72, movementScale)
+	else:
+		movementScale = 1
+		speedScale = 1
+	
 	#Handle Inputs
-	for key in directions:
-		
-		var axis = directions[key]["speed"][0]
-		
-		#Get the pressed and released buttons
-		if Input.is_action_just_pressed(key):
-			directions[key]["state"] = true
-			
-		if Input.is_action_just_released(key):
-			directions[key]["state"] = false
+	if keyDown("Up"):
+		velocity.y = move_toward(velocity.y, -speed * speedScale, acceleration * movementScale)
+	if keyDown("Down"):
+		velocity.y = move_toward(velocity.y, speed * speedScale, acceleration * movementScale)
+	if keyDown("Left"):
+		velocity.x = move_toward(velocity.x, -speed * speedScale, acceleration * movementScale)
+	if keyDown("Right"):
+		velocity.x = move_toward(velocity.x, speed * speedScale, acceleration * movementScale)
 	
-		#Accelerate if the key is pressed
-		if directions[key]["state"]:
-			var speed = directions[key]["speed"][1]
-			velocity[axis] = move_toward(velocity[axis], speed, 10)
-	
-	# Break if the keys are not pressed
-	if not directions["Up"]["state"] and not directions["Down"]["state"]:
-		velocity.y = move_toward(velocity.y, 0, Break)
-	if not directions["Left"]["state"] and not directions["Right"]["state"]:
-		velocity.x = move_toward(velocity.x, 0, Break)
+	if (!keyDown("Up") and !keyDown("Down")) or (keyDown("Up") and keyDown("Down")):
+		velocity.y = move_toward(velocity.y, 0, deceleration)
+	if !keyDown("Left") and !keyDown("Right") or (keyDown("Left") and keyDown("Right")):
+		velocity.x = move_toward(velocity.x, 0, deceleration)
 	
 	move_and_slide()
