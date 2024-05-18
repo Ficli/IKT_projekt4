fca2c42d15133288b45127d45f40138bf316db29
Lobster <polyak.daavid@gmail.com>
2024-05-18 12:35:39 +0200

    commit

--- scenes/taxi.gd
+++ scenes/taxi.gd
@@ -15,27 +15,26 @@ func CircleRange(num):
 		num = (num / 360 - int(num / 360)) * 360
 	return num
 
-func setAnimation(deg):
-	var going_right = deg > 315 or deg < 45
-	var going_left = deg < 225 and deg > 135
-	var going_up = deg > 225 and deg < 315
-	var going_down = deg > 45 and deg < 135
+func setAnimation(deg, speed):
+	var states = {
+		"right": [deg > 315 or deg < 45, "left", true],
+		"left": [deg < 225 and deg > 135, "left", false],
+		"up": [deg > 225 and deg < 315, "up", false],
+		"down": [deg > 45 and deg < 135, "down", false],
+	}
+	var forward = speed > 0
 	
-	if going_right:
-		sprite_2d.animation = "go_left"
-		sprite_2d.flip_h = true
+	for direction in states:
+		if states[direction][0]:
+			var flip_h = states[direction][2]
+			var anim = states[direction][1]
+			
+			if speed != 0: anim = "go_" + anim
+			else: anim = "stand_" + anim
+			sprite_2d.animation = anim
+			sprite_2d.flip_h = flip_h
 	
-	if going_left:
-		sprite_2d.animation = "go_left"
-	
-	if going_up:
-		sprite_2d.animation = "go_up"
-		sprite_2d.flip_h = false
-	
-	if going_down:
-		sprite_2d.animation = "go_down"
-		sprite_2d.flip_h = false
-
+			
 var angle = 0
 var turn = 2.5
 var MaxSpeed = 400
@@ -56,11 +55,16 @@ func _physics_process(delta):
 		speed = move_toward(speed, -MaxSpeed / 2, acceleration)
 	
 	# turn if the car is moving
-	if velocity.x != 0 or velocity.y != 0:
+	if speed > 0:
 		if keyDown("Left"):
 			angle = move_toward(angle, angle - 45, turn)
 		if keyDown("Right"):
 			angle = move_toward(angle, angle + 45, turn)
+	else:
+		if keyDown("Left"):
+			angle = move_toward(angle, angle + 45, turn)
+		if keyDown("Right"):
+			angle = move_toward(angle, angle - 45, turn)
 	
 	if keyDown("HandBreak"):
 		speed = move_toward(speed, 0, deceleration * 10)
@@ -68,7 +72,7 @@ func _physics_process(delta):
 	# keep the angle between 0 and 360
 	if abs(angle) == 360: angle = 0
 	
-	setAnimation(CircleRange(angle))
+	setAnimation(CircleRange(angle), speed)
 	
 	if !keyDown("Up") and !keyDown("Down"):
 		speed = move_toward(speed, 0, deceleration)
